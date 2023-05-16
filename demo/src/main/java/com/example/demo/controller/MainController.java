ae037320dba46dabfe292db06c702b0ef8a760f3
LeeDhffff <ehdgjs1311@naver.com>
2023-05-17 00:13:15 +0900

    커밋_ WEB-INF 설정

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
+	
 	@RequestMapping("/")
 	public String index() {
-		return "Hello World";
+		return "index";
 	}
 }
