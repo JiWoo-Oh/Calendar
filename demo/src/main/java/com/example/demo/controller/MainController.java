b1c12c0470967118925d34ee28414fb07bb62a49
JiWoo Oh <bear0@DESKTOP-U3MS3EH>
2023-05-22 22:35:09 +0900

    기본 세팅 및 컨트롤러 설정

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -10,4 +10,19 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
+	
+	@RequestMapping("/mypage")
+	public String mypage() {
+		return "mypage/mypage";
+	}
+	
+	@RequestMapping("/store")
+	public String store() {
+		return "store/main";
+	}
+	
+	@RequestMapping("/customercenter")
+	public String customer_center() {
+		return "cc/main";
+	}
 }
